807861834cd272020f6412b51454cab94b7c989c
Ilya Gruzhevski <ilya.gruzhevski@gmail.com>
2018-11-10 12:49:41 +0100

    Initial Commit

---
+++ NetworkLocalStreaming/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkLocalStreaming
+//
+//  Created by Ilya Gruzhevski on 10/11/2018.
+//  Copyright © 2018 Ilya Gruzhevski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
